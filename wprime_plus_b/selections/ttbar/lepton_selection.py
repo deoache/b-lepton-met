985a1fd3fa8be23b78df61c979decab6ff2b89db
deoache <daniel.ocampoh@udea.edu.co>
2023-09-04 14:08:47 +0000

    update lepton selection

--- wprime_plus_b/selections/ttbar/lepton_selection.py
+++ wprime_plus_b/selections/ttbar/lepton_selection.py
@@ -1,10 +1,15 @@
+import json
 import numpy as np
 import awkward as ak
+import importlib.resources
 from coffea.nanoevents.methods.base import NanoEventsArray
 
 
 def select_good_electrons(
-    events: NanoEventsArray, channel: str, lepton_flavor: str
+    events: NanoEventsArray,
+    electron_pt_threshold: int,
+    electron_id_wp: str,
+    electron_iso_wp: str = None,
 ) -> ak.highlevel.Array:
     """
     Selects and filters "good" electrons from a collection of events based on specified criteria.
@@ -14,38 +19,64 @@ def select_good_electrons(
     events:
         A collection of events represented using the NanoEventsArray class.
 
-    channel: {'1b1l', '1b1e1mu', '2b1l'}
-        The channel of the event.
+    electron_pt_threshold:
+        Electron transverse momentum threshold
 
-    lepton_flavor: {'ele', 'mu'}
-        The flavor of the lepton
+    electron_id_wp:
+        Electron ID working point. Available working point for the CutBased and the MVA IDs.
+        MVA: {'wp80iso', 'wp90iso', 'wp80noiso', 'wp90noiso'}
+        CutBased: {'loose', 'medium', 'tight'}
+
+    electron_iso_wp:
+        Electron ISO working point {'loose', 'medium', 'tight'}. Only used for CutBased IDs or noIso MVA IDs
 
     Returns:
     --------
         An Awkward Array mask containing the selected "good" electrons that satisfy the specified criteria.
     """
-    if channel == "1b1e1mu":
-        good_electron_pt = 55 if lepton_flavor == "mu" else 30
-    else:
-        good_electron_pt = 55 if lepton_flavor == "ele" else 30
-    return (
-        (events.Electron.pt >= good_electron_pt)
-        & (np.abs(events.Electron.eta) < 2.4)
-        & ((np.abs(events.Electron.eta) < 1.44) | (np.abs(events.Electron.eta) > 1.57))
-        & (
-            events.Electron.mvaFall17V2Iso_WP80
-            if lepton_flavor == "ele"
-            else events.Electron.mvaFall17V2Iso_WP90
-        )
-        & (
-            events.Electron.pfRelIso04_all < 0.25
-            if hasattr(events.Electron, "pfRelIso04_all")
-            else events.Electron.pfRelIso03_all < 0.25
-        )
+    # electron pT threshold
+    electron_pt_mask = events.Electron.pt >= electron_pt_threshold
+
+    # electron pseudorapidity mask
+    electron_eta_mask = (np.abs(events.Electron.eta) < 2.4) & (
+        (np.abs(events.Electron.eta) < 1.44) | (np.abs(events.Electron.eta) > 1.57)
     )
 
+    # electron ID and Iso mask
+    id_wps = {
+        # mva ID working points https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2
+        "wp80iso": events.Electron.mvaFall17V2Iso_WP80,
+        "wp90iso": events.Electron.mvaFall17V2Iso_WP90,
+        "wp80noiso": events.Electron.mvaFall17V1noIso_WP80,
+        "wp90noiso": events.Electron.mvaFall17V1noIso_WP90,
+        # cutbased ID working points https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2
+        "loose": events.Electron.cutBased == 2,
+        "medium": events.Electron.cutBased == 3,
+        "tight": events.Electron.cutBased == 4,
+    }
+    iso_wps = {
+        # https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideMuonSelection
+        "loose": events.Electron.pfRelIso04_all < 0.25
+        if hasattr(events.Electron, "pfRelIso04_all")
+        else events.Electron.pfRelIso03_all < 0.25,
+        "medium": events.Electron.pfRelIso04_all < 0.20
+        if hasattr(events.Electron, "pfRelIso04_all")
+        else events.Electron.pfRelIso03_all < 0.20,
+        "tight": events.Electron.pfRelIso04_all < 0.15
+        if hasattr(events.Electron, "pfRelIso04_all")
+        else events.Electron.pfRelIso03_all < 0.15,
+    }
+    if electron_id_wp in ["wp80iso", "wp90iso"]:
+        electron_id_mask = id_wps[electron_id_wp]
+    else:
+        electron_id_mask = (id_wps[electron_id_wp]) & (iso_wps[electron_iso_wp])
 
-def select_good_muons(events: NanoEventsArray) -> ak.highlevel.Array:
+    return (electron_pt_mask) & (electron_eta_mask) & (electron_id_mask)
+
+
+def select_good_muons(
+    events: NanoEventsArray, muon_pt_threshold: int, muon_id_wp: str, muon_iso_wp: str
+) -> ak.highlevel.Array:
     """
     Selects and filters "good" muons from a collection of events based on specified criteria.
 
@@ -54,20 +85,49 @@ def select_good_muons(events: NanoEventsArray) -> ak.highlevel.Array:
     events:
         A collection of events represented using the NanoEventsArray class.
 
+    muon_pt_threshold:
+        Muon transverse momentum threshold
+
+    muon_id_wp:
+        Muon ID working point. Available working points for the CutBased ID {'loose', 'medium', 'tight'}
+
+    muon_iso_wp:
+        Muon ISO working point {'loose', 'medium', 'tight'}
+
     Returns:
     --------
         An Awkward Array mask containing the selected "good" muons that satisfy the specified criteria.
     """
-    return (
-        (events.Muon.pt >= 35)
-        & (np.abs(events.Muon.eta) < 2.4)
-        & (events.Muon.tightId)
-        & (
-            events.Muon.pfRelIso04_all < 0.25
-            if hasattr(events.Muon, "pfRelIso04_all")
-            else events.Muon.pfRelIso03_all < 0.25
-        )
-    )
+    # muon pT threshold
+    muon_pt_mask = events.Muon.pt >= muon_pt_threshold
+
+    # electron pseudorapidity mask
+    muon_eta_mask = np.abs(events.Muon.eta) < 2.4
+
+    # muon ID mask https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2
+    id_wps = {
+        # cutbased ID working points 
+        "loose": events.Muon.looseId,
+        "medium": events.Muon.mediumId,
+        "tight": events.Muon.tightId,
+    }
+    muon_id_mask = id_wps[muon_id_wp]
+
+    # muon ID and Iso mask https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideMuonSelection
+    iso_wps = {
+        "loose": events.Muon.pfRelIso04_all < 0.25
+        if hasattr(events.Muon, "pfRelIso04_all")
+        else events.Muon.pfRelIso03_all < 0.25,
+        "medium": events.Muon.pfRelIso04_all < 0.20
+        if hasattr(events.Muon, "pfRelIso04_all")
+        else events.Muon.pfRelIso03_all < 0.20,
+        "tight": events.Muon.pfRelIso04_all < 0.15
+        if hasattr(events.Muon, "pfRelIso04_all")
+        else events.Muon.pfRelIso03_all < 0.15,
+    }
+    muon_iso_mask = id_wps[muon_iso_wp]
+
+    return (muon_pt_mask) & (muon_eta_mask) & (muon_id_mask) & (muon_iso_mask)
 
 
 def select_good_taus(events: NanoEventsArray) -> ak.highlevel.Array:
