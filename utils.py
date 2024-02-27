f3bfed795d80f59be6e978fa6b9e9b05e8fa5c8d
deoache <daniel.ocampoh@udea.edu.co>
2024-02-27 15:09:35 +0000

    fix filesets removing

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
+import subprocess
 from pathlib import Path
+
 
 def divide_list(lst: list, n: int):
     """
@@ -20,22 +22,20 @@ def divide_list(lst: list, n: int):
 
 
 def divide_fileset(sample: str, year: str, nsplit: int, facility: str):
-    # load fileset  
+    # load fileset
     main_dir = Path.cwd()
     fileset_path = Path(f"{main_dir}/wprime_plus_b/fileset")
     if facility == "lxplus":
         json_file = f"{fileset_path}/fileset_{year}_UL_NANO_lxplus.json"
     else:
-        json_file = f"{fileset_path}/fileset_{year}_UL_NANO.json" 
+        json_file = f"{fileset_path}/fileset_{year}_UL_NANO.json"
     with open(json_file, "r") as handle:
         data = json.load(handle)
         
     # make output filesets directory
     output_directory = Path(f"{fileset_path}/filesets")
     if output_directory.exists():
-        for file in output_directory.glob("*"):
-            if file.is_file():
-                file.unlink()
+        subprocess.run(["rm", f"{output_directory}/*"])
     else:
         output_directory.mkdir(parents=True)
         
@@ -47,7 +47,7 @@ def divide_fileset(sample: str, year: str, nsplit: int, facility: str):
         with open(f"{output_directory}/{sample}.json", "w") as json_file:
             json.dump(sample_data, json_file, indent=4, sort_keys=True)
     else:
-        root_files_list = divide_list(data[sample], nsplit) 
+        root_files_list = divide_list(data[sample], nsplit)
         keys = ".".join(f"{sample}_{i}" for i in range(1, nsplit + 1)).split(".")
         for key, value in zip(keys, root_files_list):
             sample_data = {}
@@ -56,41 +56,57 @@ def divide_fileset(sample: str, year: str, nsplit: int, facility: str):
             filesets[key] = f"{output_directory}/{key}.json"
             with open(f"{output_directory}/{key}.json", "w") as json_file:
                 json.dump(sample_data, json_file, indent=4, sort_keys=True)
-
     return filesets
+
 
 def get_filesets(sample: str, year: str, nsplit: int, facility: str):
     return divide_fileset(sample, year, nsplit, facility)
 
+
 def run_checker(args: dict) -> None:
     # check processor
-    available_processors = ["ttbar", "ztoll", "qcd", "btag_eff", "trigger_eff"] 
-    assert args["processor"] in available_processors, f"Incorrect processor. Available processors are: {available_processors}"
-    
+    available_processors = ["ttbar", "ztoll", "qcd", "btag_eff", "trigger_eff"]
+    assert (
+        args["processor"] in available_processors
+    ), f"Incorrect processor. Available processors are: {available_processors}"
+
     if args["processor"] == "ttbar":
         # check channel
         available_channels = ["2b1l", "1b1e1mu", "1b1l"]
-        assert args["channel"] in available_channels, f"Incorrect channel. Available channels are: {available_channels}"
+        assert (
+            args["channel"] in available_channels
+        ), f"Incorrect channel. Available channels are: {available_channels}"
         # check lepton flavor
         available_lepton_flavors = ["ele", "mu"]
-        assert args["lepton_flavor"] in available_lepton_flavors, f"Incorrect lepton flavor. Available lepton flavors are: {available_lepton_flavors}"
-        
+        assert (
+            args["lepton_flavor"] in available_lepton_flavors
+        ), f"Incorrect lepton flavor. Available lepton flavors are: {available_lepton_flavors}"
+
         # check Data sample
         if args["channel"] == "1b1e1mu":
             if args["lepton_flavor"] == "mu":
-                assert args["sample"] != "SingleMuon", "1b1e1mu muon channel should be run with SingleElectron dataset"
+                assert (
+                    args["sample"] != "SingleMuon"
+                ), "1b1e1mu muon channel should be run with SingleElectron dataset"
             else:
-                assert args["sample"] != "SingleElectron", "1b1e1mu electron channel should be run with SingleMuon dataset"
+                assert (
+                    args["sample"] != "SingleElectron"
+                ), "1b1e1mu electron channel should be run with SingleMuon dataset"
         else:
             if args["lepton_flavor"] == "mu":
-                assert args["sample"] != "SingleElectron", "2b1l muon channel should be run with SingleMuon dataset"
+                assert (
+                    args["sample"] != "SingleElectron"
+                ), "2b1l muon channel should be run with SingleMuon dataset"
             else:
-                assert args["sample"] != "SingleMuon", "2b1l electron channel should be run with SingleElectron dataset"
-                
+                assert (
+                    args["sample"] != "SingleMuon"
+                ), "2b1l electron channel should be run with SingleElectron dataset"
     if args["processor"] == "qcd":
         # check channel
         available_channels = ["A", "B", "C", "D"]
-        assert args["channel"] in available_channels, f"Incorrect channel. Available channels are: {available_channels}"
+        assert (
+            args["channel"] in available_channels
+        ), f"Incorrect channel. Available channels are: {available_channels}"
         assert (
             args["lepton_flavor"] == "mu" and args["output_type"] == "hist"
         ), "Only muon channel and histograms are available"
@@ -107,8 +123,4 @@ def manage_args(args: dict) -> dict:
     if processor in processor_args_mapping:
         for arg in processor_args_mapping[processor]:
             args[arg] = None
-            
-    # process nsample arg
-    if args.get("nsample"):
-        args["nsample"] = args["nsample"].split(",")
     return args
