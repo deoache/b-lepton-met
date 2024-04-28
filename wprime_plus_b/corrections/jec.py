2a4cff8ffb3465fb4d8fe4dbe630ee67047c760e
deoache <daniel.ocampoh@udea.edu.co>
2024-04-28 06:52:29 +0000

    change functions to update objects with corrections

--- wprime_plus_b/corrections/jec.py
+++ wprime_plus_b/corrections/jec.py
@@ -8,7 +8,7 @@ from coffea.nanoevents.methods.base import NanoEventsArray
 
 
 # Recomendations https://twiki.cern.ch/twiki/bin/viewauth/CMS/JECDataMC#Recommended_for_MC
-def jet_corrections(events: NanoEventsArray, year: str) -> Tuple[ak.Array, ak.Array]:
+def apply_jet_corrections(events: NanoEventsArray, year: str) -> None:
     """
     Apply JEC/JER corrections to jets (propagate to MET)
 
@@ -20,11 +20,7 @@ def jet_corrections(events: NanoEventsArray, year: str) -> Tuple[ak.Array, ak.Ar
         events:
             events collection
         year:
-            Year of the dataset {'2016', '2017', '2018'}
-
-    Returns:
-    --------
-        corrected jets and MET
+            Year of the dataset {'2016APV', '2016', '2017', '2018'}
     """
     # load jet and MET factories with JEC/JER corrections
     with importlib.resources.path(
@@ -44,12 +40,10 @@ def jet_corrections(events: NanoEventsArray, year: str) -> Tuple[ak.Array, ak.Ar
         return jets
 
     # get corrected jets
-    corrected_jets = factories["jet_factory"][year].build(
+    events["Jet"] = factories["jet_factory"][year].build(
         add_jec_variables(events.Jet, events.fixedGridRhoFastjetAll),
         events.caches[0],
     )
 
     # get corrected MET
-    corrected_met = factories["met_factory"].build(events.MET, corrected_jets, {})
-
-    return corrected_jets, corrected_met
+    events["MET"] = factories["met_factory"].build(events.MET, events.Jet, {})
--- wprime_plus_b/corrections/rochester.py
+++ wprime_plus_b/corrections/rochester.py
@@ -1,38 +1,48 @@
 import numpy as np
 import awkward as ak
+from wprime_plus_b.corrections.met import update_met
 from coffea.lookup_tools import txt_converters, rochester_lookup
 
 
-def apply_rochester_corrections(muons, is_mc, year):
+def apply_rochester_corrections(events, is_mc, year):
     rochester_data = txt_converters.convert_rochester_file(
         f"wprime_plus_b/data/RoccoR{year}UL.txt", loaduncs=True
     )
     rochester = rochester_lookup.rochester_lookup(rochester_data)
 
-    if is_mc:
-        hasgen = ~np.isnan(ak.fill_none(muons.matched_gen.pt, np.nan))
-        mc_rand = np.random.rand(*ak.to_numpy(ak.flatten(muons.pt)).shape)
-        mc_rand = ak.unflatten(mc_rand, ak.num(muons.pt, axis=1))
-        corrections = np.array(ak.flatten(ak.ones_like(muons.pt)))
+    # define muon pt_raw field
+    events["Muon", "pt_raw"] = ak.ones_like(events.Muon.pt) * events.Muon.pt
+    
+    if is_mc:    
+        hasgen = ~np.isnan(ak.fill_none(events.Muon.matched_gen.pt, np.nan))
+        mc_rand = np.random.rand(*ak.to_numpy(ak.flatten(events.Muon.pt)).shape)
+        mc_rand = ak.unflatten(mc_rand, ak.num(events.Muon.pt, axis=1))
+        corrections = np.array(ak.flatten(ak.ones_like(events.Muon.pt)))
         mc_kspread = rochester.kSpreadMC(
-            muons.charge[hasgen],
-            muons.pt[hasgen],
-            muons.eta[hasgen],
-            muons.phi[hasgen],
-            muons.matched_gen.pt[hasgen],
+            events.Muon.charge[hasgen],
+            events.Muon.pt[hasgen],
+            events.Muon.eta[hasgen],
+            events.Muon.phi[hasgen],
+            events.Muon.matched_gen.pt[hasgen],
         )
         mc_ksmear = rochester.kSmearMC(
-            muons.charge[~hasgen],
-            muons.pt[~hasgen],
-            muons.eta[~hasgen],
-            muons.phi[~hasgen],
-            muons.nTrackerLayers[~hasgen],
+            events.Muon.charge[~hasgen],
+            events.Muon.pt[~hasgen],
+            events.Muon.eta[~hasgen],
+            events.Muon.phi[~hasgen],
+            events.Muon.nTrackerLayers[~hasgen],
             mc_rand[~hasgen],
         )
         hasgen_flat = np.array(ak.flatten(hasgen))
         corrections[hasgen_flat] = np.array(ak.flatten(mc_kspread))
         corrections[~hasgen_flat] = np.array(ak.flatten(mc_ksmear))
-        corrections = ak.unflatten(corrections, ak.num(muons.pt, axis=1))
+        corrections = ak.unflatten(corrections, ak.num(events.Muon.pt, axis=1))
     else:
-        corrections = rochester.kScaleDT(muons.charge, muons.pt, muons.eta, muons.phi)
-    return muons.pt * corrections
+        corrections = rochester.kScaleDT(events.Muon.charge, events.Muon.pt, events.Muon.eta, events.Muon.phi)
+    
+    # get rochester pT for Muon
+    events["Muon", "pt_rochester"] = events.Muon.pt * corrections
+    # update muon pT field
+    events["Muon", "pt"] = events.Muon.pt_rochester
+    # propagate muon pT corrections to MET
+    update_met(events=events, lepton="Muon")    
