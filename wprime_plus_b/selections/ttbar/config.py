0085b14bc73e43ad871c646c8a65fb2b034cfcb5
deoache <daniel.ocampoh@udea.edu.co>
2024-04-29 08:22:36 +0000

    change electron Iso for 1b1l

--- wprime_plus_b/selections/ttbar/config.py
+++ wprime_plus_b/selections/ttbar/config.py
@@ -27,12 +27,12 @@ ttbar_electron_selection = {
         "ele": {
             "electron_pt_threshold": 55,
             "electron_id_wp": "wp90iso",
-            "electron_iso_wp": "tight",
+            "electron_iso_wp": None,
         },
         "mu": {
             "electron_pt_threshold": 50,
             "electron_id_wp": "wp90iso",
-            "electron_iso_wp": "tight",
+            "electron_iso_wp": None,
         },
     },
 }
